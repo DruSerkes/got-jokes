81e4fe6ad912704448e087d5a19dd668f5f50a59
Dru Serkes <DruSerkes@gmail.com>
2020-12-31 02:27:00 -0500

    add jokeData interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
+import { useState, useEfect } from 'react';
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
-function App() {
+interface jokeData {
+  "error": boolean;
+  "category": string;
+  "type": string;
+  "joke": string;
+  "flags": {
+    "nsfw": boolean,
+    "religious": boolean,
+    "political": boolean,
+    "racist": boolean,
+    "sexist": boolean,
+    "explicit": boolean
+  },
+  "id": number;
+  "safe": boolean;
+  "lang": string;
+}
+
+
+export const App = () => {
+  const [joke, setJoke] = useState(initialState)
+
   return (
     <div className="App">
       <h1>Got Jokes</h1>
+      <Button variant="primary">Make Me Laugh!</Button>
+
       {/* TODO App goes here */}
     </div>
   );
 }
-
-export default App;
