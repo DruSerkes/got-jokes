8be4ba9fd3ebeb6acfb15d6d58f3b8492bf90535
Dru Serkes <DruSerkes@gmail.com>
2021-01-02 01:14:15 -0500

    work on app tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,46 +1,66 @@
 import * as React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor, waitForDomChange } from '@testing-library/react';
 import { App } from './App';
 import axios from 'axios';
-
+import { act } from 'react-dom/test-utils';
 
 describe('App tests', () => {
 
   const testJoke = {
-    data: {
-      "error": false,
-      "category": 'dark',
-      "type": 'single',
-      "joke": 'test joke',
-      "flags": {
-        "nsfw": false,
-        "religious": false,
-        "political": false,
-        "racist": false,
-        "sexist": false,
-        "explicit": false
-      },
-      "id": 13,
-      "safe": true,
-      "lang": 'en',
-      'setup': 'test setup',
-      'delivery': 'test delivery'
-    }
+    "error": false,
+    "category": 'dark',
+    "type": 'single',
+    "joke": 'test joke',
+    "flags": {
+      "nsfw": false,
+      "religious": false,
+      "political": false,
+      "racist": false,
+      "sexist": false,
+      "explicit": false
+    },
+    "id": 13,
+    "safe": true,
+    "lang": 'en',
+    'setup': 'test setup',
+    'delivery': 'test delivery'
   };
+  const testResp = { data: testJoke }
 
-  // Spy on axios.get and mock it's implementation
-  const spy = jest.spyOn(axios, 'get');
-  spy.mockResolvedValue(testJoke);
+  // Mock axios
+  const spy = jest.spyOn(axios, "get");
+  // jest.mock('axios');
+  spy.mockResolvedValue(testResp)
+
 
   it('renders without breaking and matches snapshot', () => {
     const { asFragment } = render(<App />);
     expect(asFragment()).toMatchSnapshot();
   });
 
-  it('GETs a joke with axios on mount', () => {
+  it('GETs a joke with axios on mount', async () => {
+    // spy.mockResolvedValue(testResp)
+    // act(() => {
     render(<App />);
+    // })
     expect(spy).toHaveBeenCalledTimes(1);
   });
+
+  // it('should call axios.get again when you click the button', async () => {
+  //   // act(() => { render(<App />) });
+  //   render(<App />);
+  //   const btn = screen.getByText('Make Me Laugh!');
+  //   expect(btn).toBeInTheDocument();
+  //   expect(spy).toHaveBeenCalledTimes(1);
+  //   act(() => {
+  //     fireEvent.click(btn);
+  //     fireEvent.click(btn);
+  //   });
+  //   await waitFor(() => {
+  //     fireEvent.click(btn);
+  //   })
+  //   expect(spy).toHaveBeenCalledTimes(2);
+  // })
 })
 
 
