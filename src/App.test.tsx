359fb3b9a3e8d45d40c72a0a571ecafe8f3c8238
Dru Serkes <DruSerkes@gmail.com>
2021-01-02 00:17:34 -0500

    get App tests working

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import * as React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { App } from './App';
+import axios from 'axios';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('App tests', () => {
+
+  const testJoke = {
+    data: {
+      "error": false,
+      "category": 'dark',
+      "type": 'single',
+      "joke": 'test joke',
+      "flags": {
+        "nsfw": false,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": false
+      },
+      "id": 13,
+      "safe": true,
+      "lang": 'en',
+      'setup': 'test setup',
+      'delivery': 'test delivery'
+    }
+  };
+
+  // Spy on axios.get and mock it's implementation
+  const spy = jest.spyOn(axios, 'get');
+  spy.mockResolvedValue(testJoke);
+
+  it('renders without breaking and matches snapshot', () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('GETs a joke with axios on mount', () => {
+    render(<App />);
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+})
+
+
