93f49ce64cabe89b2fb7ca8058c2ab05e5b3ab36
Dru Serkes <DruSerkes@gmail.com>
2021-01-01 23:26:23 -0500

    add Cols and margins

--- src/Joke/Joke.tsx
+++ src/Joke/Joke.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { jokeData } from '../types';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 interface JokeProps {
     isLoading: boolean;
@@ -14,19 +15,25 @@ export const Joke = ({ joke, isLoading }: JokeProps) => {
             {
                 !isLoading && joke?.type === 'twopart' &&
                 <Container className='Joke'>
-                    <Row className='Joke-Setup'>
-                        {joke?.setup}
+                    <Row className='Joke-Setup my-3'>
+                        <Col>
+                            {joke?.setup}
+                        </Col>
                     </Row>
-                    <Row className='Joke-Delivery'>
-                        {joke?.delivery}
+                    <Row className='Joke-Delivery my-3'>
+                        <Col>
+                            {joke?.delivery}
+                        </Col>
                     </Row>
                 </Container>
             }
             {
                 !isLoading && joke?.type === 'single' &&
                 <Container className='Joke'>
-                    <Row>
-                        {joke?.joke}
+                    <Row className='my-3'>
+                        <Col>
+                            {joke?.joke}
+                        </Col>
                     </Row>
                 </Container>
             }
