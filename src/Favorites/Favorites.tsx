998590c91d424a3f513219f9cf10c51d63077c65
Dru Serkes <DruSerkes@gmail.com>
2021-01-02 12:35:51 -0500

    add empty Favorites component

---
+++ src/Favorites/Favorites.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { jokeData } from '../types';
+
+interface FavoritesProps {
+    favorites?: jokeData[]
+}
+
+export const Favorites = ({ favorites }: FavoritesProps) => { }
