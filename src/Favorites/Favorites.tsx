cff83cad483af87cd55671b065fd01db6a298fdb
Dru Serkes <DruSerkes@gmail.com>
2021-01-03 23:33:21 -0500

    remove redundant comments/code

--- src/Favorites/Favorites.tsx
+++ src/Favorites/Favorites.tsx
@@ -8,31 +8,12 @@ interface FavoritesProps {
 }
 
 export const Favorites = ({ favorites, viewingFavorites }: FavoritesProps) => {
-    // const generateFav = (joke: jokeData) => {
-    //     if (joke.type === 'twopart') {
-    //         return (
-    //             <li className='py-3 list-group-item-dark border-bottom rounded border-dark'>
-    //                 {joke?.setup}
-    //                 <br />
-    //                 {joke?.delivery}
-    //             </li>
-    //         );
-    //     } else {
-    //         return (
-    //             <li className='py-3 list-group-item-dark border-bottom rounded border-dark'>
-    //                 {joke?.joke}
-    //             </li>
-    //         );
-    //     };
-    // };
-
     return (
         <>
             { viewingFavorites &&
                 (<>
                     <h3>Favorites</h3>
                     <ul className='Favorites rounded list-group my-3'>
-                        {/* {favorites?.map(fav => generateFav(fav))} */}
                         {favorites?.map(fav => <Favorite fav={fav} />)}
                     </ul>
                 </>
