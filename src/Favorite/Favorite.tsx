6978cb8e94e23a2dd18a2c6a53b5e1bd44173117
Dru Serkes <DruSerkes@gmail.com>
2021-01-03 23:27:28 -0500

    create Favorite component

---
+++ src/Favorite/Favorite.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { jokeData } from '../types';
+
+interface FavoritesProps {
+    fav: jokeData;
+}
+
+export const Favorite = ({ fav }: FavoritesProps) => {
+    const generateFav = (joke: jokeData) => {
+        if (joke.type === 'twopart') {
+            return (
+                <li className='py-3 list-group-item-dark border-bottom rounded border-dark'>
+                    {joke?.setup}
+                    <br />
+                    {joke?.delivery}
+                </li>
+            );
+        } else {
+            return (
+                <li className='py-3 list-group-item-dark border-bottom rounded border-dark'>
+                    {joke?.joke}
+                </li>
+            );
+        };
+    };
+
+    return generateFav(fav);
+}
