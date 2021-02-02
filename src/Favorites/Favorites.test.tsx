0b7348d4a57c1b575543a7841e421b684fe4f9b1
Dru Serkes <DruSerkes@gmail.com>
2021-02-01 22:56:18 -0500

    grab testJoke from setup

--- src/Favorites/Favorites.test.tsx
+++ src/Favorites/Favorites.test.tsx
@@ -1,29 +1,11 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import { Favorites } from './Favorites';
+import { testJoke } from '../setupTests';
 
 
 
 describe('Favorites tests', () => {
-    const testJoke = {
-        "error": false,
-        "category": 'dark',
-        "type": 'single',
-        "joke": 'test joke',
-        "flags": {
-            "nsfw": false,
-            "religious": false,
-            "political": false,
-            "racist": false,
-            "sexist": false,
-            "explicit": false
-        },
-        "id": 13,
-        "safe": true,
-        "lang": 'en',
-        'setup': 'test setup',
-        'delivery': 'test delivery'
-    };
 
     it('should render and match snapshot', () => {
         const { asFragment } = render(<Favorites favorites={[testJoke]} viewingFavorites />);
