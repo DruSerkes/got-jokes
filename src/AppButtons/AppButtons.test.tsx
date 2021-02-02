c900ed07f6164560b09f740eca5a47846c59c84a
Dru Serkes <DruSerkes@gmail.com>
2021-02-01 23:00:24 -0500

    cleanup test

--- src/AppButtons/AppButtons.test.tsx
+++ src/AppButtons/AppButtons.test.tsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import { AppButtons } from './AppButtons';
-import { testJoke } from '../setupTests';
+import { testJoke, setJoke, setViewingFavorites, setFavorites, clearFavorites } from '../setupTests';
 
 describe('AppButtons tests', () => {
-    const setJoke = jest.fn(() => console.log('setting joke'));
-    const setViewingFavorites = jest.fn(() => console.log('viewing favorites'));
-    const setFavorites = jest.fn(() => console.log('setting favorites'));
-    const clearFavorites = jest.fn(() => console.log('clearing favorites'));
 
     it('should render and match snapshot', () => {
         const { asFragment } = render(<AppButtons viewingFavorites={false} isLoading={false} joke={testJoke} favorites={[]} setJoke={setJoke} setViewingFavorites={setViewingFavorites} setFavorites={setFavorites} clearFavorites={clearFavorites} />);
