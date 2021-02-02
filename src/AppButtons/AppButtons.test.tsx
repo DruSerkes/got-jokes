bf45dbfa494fb851d637c49e6318b2795eb25f90
Dru Serkes <DruSerkes@gmail.com>
2021-02-01 23:14:40 -0500

    add more test cases

--- src/AppButtons/AppButtons.test.tsx
+++ src/AppButtons/AppButtons.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { AppButtons } from './AppButtons';
 import { testJoke, setJoke, setViewingFavorites, setFavorites, clearFavorites } from '../setupTests';
 
@@ -8,6 +8,21 @@ describe('AppButtons tests', () => {
     it('should render and match snapshot', () => {
         const { asFragment } = render(<AppButtons viewingFavorites={false} isLoading={false} joke={testJoke} favorites={[]} setJoke={setJoke} setViewingFavorites={setViewingFavorites} setFavorites={setFavorites} clearFavorites={clearFavorites} />);
         expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('should call setJoke and setViewingFavorites', () => {
+        const { getByText } = render(<AppButtons viewingFavorites={false} isLoading={false} joke={testJoke} favorites={[]} setJoke={setJoke} setViewingFavorites={setViewingFavorites} setFavorites={setFavorites} clearFavorites={clearFavorites} />);
+        const getJokeButton = getByText('Make Me Laugh!');
+        fireEvent.click(getJokeButton);
+        expect(setJoke).toHaveBeenCalledTimes(1);
+        expect(setViewingFavorites).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call setVewingFavorites', () => {
+        const { getByText } = render(<AppButtons viewingFavorites={false} isLoading={false} joke={testJoke} favorites={[]} setJoke={setJoke} setViewingFavorites={setViewingFavorites} setFavorites={setFavorites} clearFavorites={clearFavorites} />);
+        const showFavoritesButton = getByText('Show my Favorites');
+        fireEvent.click(showFavoritesButton);
+        expect(setViewingFavorites).toHaveBeenCalledTimes(1);
     });
 
 }); 
