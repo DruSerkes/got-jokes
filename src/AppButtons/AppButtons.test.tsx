60f2eacaba756df63d5e04c5a221538066fe7cc6
Dru Serkes <DruSerkes@gmail.com>
2021-02-01 22:53:49 -0500

    add AppButtons test

---
+++ src/AppButtons/AppButtons.test.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { AppButtons } from './AppButtons';
+import { testJoke } from '../setupTests';
+
+describe('AppButtons tests', () => {
+    export const testJoke = {
+        "error": false,
+        "category": 'dark',
+        "type": 'single',
+        "joke": 'test joke',
+        "flags": {
+            "nsfw": false,
+            "religious": false,
+            "political": false,
+            "racist": false,
+            "sexist": false,
+            "explicit": false
+        },
+        "id": 13,
+        "safe": true,
+        "lang": 'en',
+        'setup': 'test setup',
+        'delivery': 'test delivery'
+    };
+    const setJoke = jest.fn(() => console.log('setting joke'));
+    const setViewingFavorites = jest.fn(() => console.log('viewing favorites'));
+    const setFavorites = jest.fn(() => console.log('setting favorites'));
+    const clearFavorites = jest.fn(() => console.log('clearing favorites'));
+
+    it('should render and match snapshot', () => {
+        const { asFragment } = render(<AppButtons viewingFavorites={false} isLoading={false} joke={testJoke} favorites={[]} setJoke={setJoke} setViewingFavorites={setViewingFavorites} setFavorites={setFavorites} clearFavorites={clearFavorites} />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+}); 
