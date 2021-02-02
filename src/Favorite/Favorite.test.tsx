0efec4eca46a1428d0e53e3018a3b14431060816
Dru Serkes <DruSerkes@gmail.com>
2021-02-02 00:03:17 -0500

    test button click

--- src/Favorite/Favorite.test.tsx
+++ src/Favorite/Favorite.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { Favorite } from './Favorite';
 import { testJoke, clearFavorites } from '../setupTests';
 
@@ -14,5 +14,12 @@ describe('Favorites tests', () => {
     it('should show a joke', () => {
         const { getByText } = render(<Favorite fav={testJoke} clearFavorites={clearFavorites} />);
         expect(getByText(testJoke.joke)).toBeInTheDocument();
+    });
+
+    it('should call clearFavorites', () => {
+        const { getByText } = render(<Favorite fav={testJoke} clearFavorites={clearFavorites} />);
+        const removeButton = getByText('X');
+        fireEvent.click(removeButton);
+        expect(clearFavorites).toHaveBeenCalledTimes(1);
     });
 })
