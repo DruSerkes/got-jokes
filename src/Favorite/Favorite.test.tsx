8acc58bbebbc3f6ab109b593a0454be43cb9345c
Dru Serkes <DruSerkes@gmail.com>
2021-02-01 23:32:02 -0500

    update props for test

--- src/Favorite/Favorite.test.tsx
+++ src/Favorite/Favorite.test.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
 import { Favorite } from './Favorite';
-import { testJoke } from '../setupTests';
+import { testJoke, clearFavorites } from '../setupTests';
 
 
 describe('Favorites tests', () => {
 
     it('should render and match snapshot', () => {
-        const { asFragment } = render(<Favorite fav={testJoke} />);
+        const { asFragment } = render(<Favorite fav={testJoke} clearFavorites={clearFavorites} />);
         expect(asFragment()).toMatchSnapshot();
     });
 
     it('should show a joke', () => {
-        const { getByText } = render(<Favorite fav={testJoke} />);
+        const { getByText } = render(<Favorite fav={testJoke} clearFavorites={clearFavorites} />);
         expect(getByText(testJoke.joke)).toBeInTheDocument();
     });
 })
