678577b5df3806ccda91ee2861382792a34e60ca
Dru Serkes <DruSerkes@gmail.com>
2021-02-06 15:50:37 -0500

    add new TODOS

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,10 @@ import Container from 'react-bootstrap/esm/Container';
 
 // TODO 
 // 1. Test exponential backoff 
+// 2. Add tweet joke button (and test) 
+// href=`https://twitter.com/intent/tweet?text=${JOKE_TEXT}`
+// 3. Add copy joke button (and test)
+// 4. Add footer (and test)
 export const App = () => {
   const [joke, setJoke] = useState<jokeData | undefined>(undefined);
   const [favorites, setFavorites] = useLocalStorage('favorites');
