fe9b021a994cdb42579277b16946f83e48adb3d6
Dru Serkes <DruSerkes@gmail.com>
2021-02-10 08:34:57 -0500

    wrap disclaimer in paragraph tag

--- src/Disclaimer/Disclaimer.tsx
+++ src/Disclaimer/Disclaimer.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 
 export const Disclaimer = () => {
     return (
-        <small className="mb-4">
-            <strong>Disclaimer:</strong> Jokes are being served via <a href="https://v2.jokeapi.dev/" target="_blank" rel="noopener noreferrer">JokeAPI</a>
-            <br />
-            Please report any offensive, racist, sexist, or generally horrible / unfunny content
-        </small>
+        <p>
+            <small className="mb-4">
+                <strong>Disclaimer:</strong> Jokes are being served via <a href="https://v2.jokeapi.dev/" target="_blank" rel="noopener noreferrer">JokeAPI</a>
+                <br />
+                Please report any offensive, racist, sexist, or generally horrible / unfunny content
+            </small>
+        </p>
     )
 };
